8bda5e492f45b2a3c8622382ca4fd1365b955538
KlaasHagge <klaash@ppedv.de>
2021-04-13 14:42:56 +0200

    Grundlagen, Controls (kommentiert)

--- HalloWPF/MainWindow.xaml.cs
+++ HalloWPF/MainWindow.xaml.cs
@@ -22,11 +22,15 @@ namespace HalloWPF
     {
         public MainWindow()
         {
+            //Dies verweist auf eine Methode in der (versteckten) automatisch generierten 2.Klassen-Datei (*.g.i.cs),
+            //welche für das Rendering des XAML-Codes verantwortlich ist. InitializeComponent() erstellt die
+            //Steuerelement-Objekte und muss daher als erste Methode des Konstruktors bestehen bleiben
             InitializeComponent();
         }
 
         private void Btn_BeispielButton_Click(object sender, RoutedEventArgs e)
         {
+            //Ändern einer UI-Property (Hier der Inhalt des Buttons)
             Btn_BeispielButton.Content = "Ich wurde angeklickt";
         }
     }
