5c0ee4f0ba8dc6d7ff0a59430b94604bb8fdc084
KlaasHagge <klaash@ppedv.de>
2021-04-14 15:33:59 +0200

    Personendialog-DataBinding

---
+++ Personendatenbank/GenderToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Personendatenbank
+{
+    public class GenderToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? parameter : Binding.DoNothing;
+
+            if ((bool)value)
+                return parameter;
+            else
+                return Binding.DoNothing;
+        }
+    }
+}
